af0cdf6700a72b30f762aeab6568b2868a0dce90
MatiasMonasterio <matias_monasterio@outlook.com>
2022-04-27 17:38:44 -0300

    feat: 🎸 add track types

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,4 +3,20 @@ export interface Artist {
   name: string;
   artwork: string;
   tracksCount: number;
+  tracks: Track[];
+  profile?: string;
+}
+
+export interface Track {
+  artists: Array<{ id: number; name: string; slug: string }>;
+  bpm: number;
+  released: number;
+  genres: Array<{ id: number; name: string; slug: string }>;
+  artwork: string;
+  key: string;
+  label: { id: number; name: string; slug: string };
+  mix: string;
+  remixers: Array<{ id: number; name: string; slug: string }>;
+  name: string;
+  preview: string;
 }
