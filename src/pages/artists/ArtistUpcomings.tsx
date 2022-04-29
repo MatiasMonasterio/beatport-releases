8169f1a1f8494589bb0b0cf01d0e55ec813ec0ec
MatiasMonasterio <matias_monasterio@outlook.com>
2022-04-29 17:03:17 -0300

    feat: 🎸 add artist releases upcoming page

---
+++ src/pages/artists/ArtistUpcomings.tsx
@@ -0,0 +1,31 @@
+import type { Track } from "types";
+
+import { useState, useEffect } from "react";
+import { Heading, Flex, Container } from "@chakra-ui/react";
+
+import { TrackCard } from "components";
+import { getArtistUpcomings } from "services/artists";
+
+export default function ArtistsReleasesUpcoming(): JSX.Element {
+  const [upcomings, setUpcomings] = useState<Track[]>([]);
+
+  useEffect(() => {
+    getArtistUpcomings().then((upcomings) => {
+      upcomings && setUpcomings(upcomings);
+    });
+  }, []);
+
+  return (
+    <Container maxW="container.xl" mt={24}>
+      <Heading as="h2" size="lg" mb={10}>
+        Upcoming
+      </Heading>
+
+      <Flex direction="column" gap={2}>
+        {upcomings.map((upcoming) => (
+          <TrackCard {...upcoming} key={upcoming.name} />
+        ))}
+      </Flex>
+    </Container>
+  );
+}
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -5,6 +5,7 @@ import { DashboardLayout } from "layouts";
 import Artists from "pages/artists/Artists";
 import ArtistById from "pages/artists/ArtistsById";
 import ArtistReleases from "pages/artists/ArtistsReleases";
+import ArtistUpcomings from "pages/artists/ArtistUpcomings";
 
 export default function AppRouter() {
   return (
@@ -17,6 +18,7 @@ export default function AppRouter() {
             <Route path="" element={<Artists />} />
             <Route path=":id" element={<ArtistById />} />
             <Route path="releases" element={<ArtistReleases />} />
+            <Route path="upcoming" element={<ArtistUpcomings />} />
           </Route>
         </Routes>
       </DashboardLayout>
