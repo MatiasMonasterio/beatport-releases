66b4fd7aae2d0336317d34e98d48733e33b5ac52
MatiasMonasterio <matias_monasterio@outlook.com>
2022-05-10 21:49:12 -0300

    feat: 🎸 create label services, add getLabels and addLabelId

---
+++ src/services/labels.ts
@@ -0,0 +1,47 @@
+import type { Label } from "types";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+type LabelsResponse = {
+  labels: Label[];
+  error?: string;
+};
+
+type LabelResponse = {
+  label: Label;
+  error?: string;
+};
+
+export const getLabels = async (): Promise<Label[] | null> => {
+  try {
+    const resp: Response = await fetch(`${API_URL}/api/labels`);
+    const { labels, error }: LabelsResponse = await resp.json();
+
+    if (error) throw new Error(error);
+    return labels;
+  } catch (error: unknown) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const addLabelId = async (beatportId: string): Promise<Label | null> => {
+  try {
+    const resp: Response = await fetch(`${API_URL}/api/labels`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ beatportId }),
+    });
+
+    const { label, error }: LabelResponse = await resp.json();
+    if (error) throw new Error(error);
+
+    return label;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
