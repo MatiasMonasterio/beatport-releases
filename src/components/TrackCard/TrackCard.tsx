1565fb5e2134f0eb32287dcffccff4fa1abb2c9c
MatiasMonasterio <matias_monasterio@outlook.com>
2022-05-14 13:50:28 -0300

    refactor: 💡 add play action, use player context, styles changes

--- src/components/TrackCard/TrackCard.tsx
+++ src/components/TrackCard/TrackCard.tsx
@@ -2,9 +2,10 @@ import type { Track } from "types";
 
 import { useState, Suspense, useMemo } from "react";
 import { Link as ReactLink } from "react-router-dom";
-import { Box, Text, Image, Grid, GridItem, Link, HStack } from "@chakra-ui/react";
+import { Box, Text, Image, Grid, GridItem, Link, HStack, Button } from "@chakra-ui/react";
 
 import { useWidget } from "context/widget";
+import { usePlayer } from "context/player";
 import { getYoutubeVideoId } from "services/widget";
 import { getDiffDays } from "utils";
 
@@ -17,7 +18,8 @@ export default function TrackCard(props: Track): JSX.Element {
   const { name, artwork, artists, label, genres, released, bpm, mix } = props;
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const { showYoutubeWidget, showBeatportWidget } = useWidget();
+  const { showYoutubeWidget } = useWidget();
+  const { playTrack, track: currentTrack } = usePlayer();
 
   const daysPassed: number = useMemo(() => {
     return getDiffDays(released);
@@ -25,6 +27,7 @@ export default function TrackCard(props: Track): JSX.Element {
 
   const publishDate: string = useMemo(() => {
     if (daysPassed === 0) return "Today";
+    if (daysPassed === 1) return `${daysPassed} day ago`;
     if (daysPassed > 0) return `${daysPassed} days ago`;
 
     return new Intl.DateTimeFormat("es-ES").format(new Date(released));
@@ -40,39 +43,52 @@ export default function TrackCard(props: Track): JSX.Element {
   };
 
   const handleShowBeatport = (): void => {
-    showBeatportWidget(props);
+    playTrack(props);
   };
 
   return (
     <Box
       _hover={{ bgColor: "gray.700" }}
+      bg={currentTrack.name === name ? "rgba(0,0,0,0.12)" : ""}
       borderRadius="sm"
       overflow="hidden"
       transition="background-color 0.1s"
     >
       <Grid templateColumns="90px 1fr 250px 100px 150px" gap={6}>
         <GridItem mb="auto" pos="relative">
-          <Image src={artwork} boxSize={24} />
+          <Button variant="link" d="block">
+            <Image src={artwork} boxSize={24} onClick={handleShowBeatport} />
 
-          {isLoading && (
-            <Suspense fallback={null}>
-              <LoadingBg />
-            </Suspense>
-          )}
+            {isLoading && (
+              <Suspense fallback={null}>
+                <LoadingBg />
+              </Suspense>
+            )}
+          </Button>
         </GridItem>
         <GridItem py={2}>
-          <Text>
-            {name} {mix} [
+          <Box color={currentTrack.name === name ? "#01FF95" : ""}>
+            <Text as="span">
+              {name} {mix}
+            </Text>{" "}
+            [
             <Link as={ReactLink} to={`/label/${label.id}`}>
               {label.name}
             </Link>
             ]
-          </Text>
+          </Box>
 
           <HStack fontSize="sm" color="gray.400" spacing={1} mb={2}>
             {artists.map((artist) => (
-              <Link as={ReactLink} key={artist.id} to={`/artist/${artist.id}`}>
-                {artist.name},
+              <Link
+                as={ReactLink}
+                key={artist.id}
+                to={`/artist/${artist.id}`}
+                _notLast={{
+                  _after: { content: `", "` },
+                }}
+              >
+                {artist.name}
               </Link>
             ))}
           </HStack>
