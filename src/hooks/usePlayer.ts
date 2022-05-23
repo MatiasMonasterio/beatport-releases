d0df5f705a822303b1dc49e901046bfd843009c1
MatiasMonasterio <matias_monasterio@outlook.com>
2022-05-22 21:32:51 -0300

    refactor: 💡 rm isPlaying value from hook and use from context

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -3,16 +3,11 @@ import { useEffect, useState } from "react";
 interface usePlayerResponse {
   duration: number;
   currentTime: number;
-  isPlaying: boolean;
 }
 
 export const usePlayer = (audioRef: React.RefObject<HTMLAudioElement>): usePlayerResponse => {
   const [duration, setDuration] = useState<number>(0);
   const [currentTime, setCurrentTime] = useState<number>(0);
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
-
-  const handlePlay = (): void => setIsPlaying(true);
-  const handlePause = (): void => setIsPlaying(false);
 
   const handleLoadMetadata = (): void => {
     setDuration(audioRef.current?.duration || 0);
@@ -23,18 +18,14 @@ export const usePlayer = (audioRef: React.RefObject<HTMLAudioElement>): usePlaye
   };
 
   useEffect(() => {
-    audioRef.current?.addEventListener("play", handlePlay);
-    audioRef.current?.addEventListener("pause", handlePause);
     audioRef.current?.addEventListener("loadedmetadata", handleLoadMetadata);
     audioRef.current?.addEventListener("timeupdate", handleTimeUpdate);
 
     return () => {
-      audioRef.current?.removeEventListener("play", handlePlay);
-      audioRef.current?.removeEventListener("pause", handlePause);
       audioRef.current?.removeEventListener("loadedmetadata", handleLoadMetadata);
       audioRef.current?.removeEventListener("timeupdate", handleTimeUpdate);
     };
   });
 
-  return { duration, currentTime, isPlaying };
+  return { duration, currentTime };
 };
