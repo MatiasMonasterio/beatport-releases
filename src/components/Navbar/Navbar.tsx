005ce6f97d88b74d4f511cfb1b5c812c08255411
MatiasMonasterio <matias_monasterio@outlook.com>
2022-05-23 00:14:17 -0300

    feat: 🎸 add prev navigation button to mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,18 @@
-import { Box, Container, Image, Text, Flex } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { Box, Container, Image, Text, Flex, Button } from "@chakra-ui/react";
+import { BiArrowBack } from "react-icons/bi";
 
 interface Props {
   isSolid: boolean;
 }
 
 export default function Navbar({ isSolid }: Props) {
+  const navigate = useNavigate();
+
+  const handleNavigateBack = () => {
+    navigate(-1);
+  };
+
   return (
     <Box position="sticky" top={0} zIndex={2}>
       <Box
@@ -15,13 +23,30 @@ export default function Navbar({ isSolid }: Props) {
         py={4}
         zIndex={100}
         right={0}
-        borderBottom="1px solid"
-        borderColor={isSolid ? "gray.700" : "transparent"}
         bgColor={isSolid ? "gray.800" : "transparent"}
       >
         <Container maxW="container.xl">
+          <Button
+            variant="link"
+            fontSize="2xl"
+            color="gray.300"
+            justifyContent="left"
+            onClick={handleNavigateBack}
+            display={{ base: "flex", sm: "none" }}
+          >
+            <BiArrowBack />
+          </Button>
+
           <Flex justify="end">
-            <Flex gap={2} alignItems="center" bg="gray.900" w={190} p={1} borderRadius="full">
+            <Flex
+              gap={2}
+              alignItems="center"
+              bg="gray.900"
+              w={190}
+              p={1}
+              borderRadius="full"
+              display={{ base: "none", sm: "flex" }}
+            >
               <Image
                 borderRadius="full"
                 boxSize="28px"
