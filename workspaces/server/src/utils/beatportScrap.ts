391da4ff3d9c3dfddc4f393944e0e6964bf2fe1d
MatiasMonasterio <matias_monasterio@outlook.com>
2022-06-05 14:58:28 -0300

    feat(workspaces/server): add labels scraper

--- workspaces/server/src/utils/beatportScrap.ts
+++ workspaces/server/src/utils/beatportScrap.ts
@@ -1,4 +1,4 @@
-import type { Artist, Track } from "@br/core";
+import type { Artist, Label, Track } from "@br/core";
 import type { TrackResponse } from "../types";
 
 import dayjs from "dayjs";
@@ -62,6 +62,55 @@ const artistsScrapper = async (artistsId: number[]): Promise<Artist[]> => {
   return artists.sort((a, b) => a.name.localeCompare(b.name));
 };
 
+const labelsScrapper = async (labelsId: number[]): Promise<Label[]> => {
+  const labels = await Promise.all(
+    labelsId.map(async (id) => {
+      const endDate = dayjs(new Date()).add(60, "day").format("YYYY-MM-DD");
+      const startDate = dayjs(new Date()).subtract(30, "day").format("YYYY-MM-DD");
+
+      const labelProfile = `${BEATPORT_BASE_URL}/label/l/${id}`;
+      const labelReleasesPage = `${labelProfile}/tracks?start-date=${startDate}&end-date=${endDate}`;
+
+      const { data }: { data: string } = await axios.get(labelReleasesPage);
+      const $ = cheerio.load(data);
+
+      const artwork = $("img.interior-top-artwork").attr("src") || "";
+      const labelName = $(".interior-title h1").get()[0].children[0] as { data: string };
+
+      const scriptData = $("script#data-objects").get()[0].children[0] as { data: string };
+      const findAndClear = findTextAndReturnRemainder(scriptData.data, "window.Playables =");
+
+      const { tracks: tracksResp }: { tracks: TrackResponse[] } = JSON.parse(findAndClear);
+
+      const tracks: Track[] = tracksResp.map((track) => ({
+        id: track.id,
+        artists: track.artists,
+        bpm: track.bpm,
+        released: new Date(track.date.released).getTime(),
+        genres: track.genres,
+        artwork: track.images.small.url.replace("1400x1400", "100x100"),
+        key: track.key,
+        label: track.label,
+        mix: track.mix,
+        remixers: track.remixers,
+        name: track.name,
+        preview: track.preview.mp3.url,
+      }));
+
+      return {
+        name: labelName.data,
+        id: id,
+        profile: labelProfile,
+        artwork,
+        tracks,
+      };
+    })
+  );
+
+  return labels.sort((a, b) => a.name.localeCompare(b.name));
+};
+
 export default {
   artists: artistsScrapper,
+  labels: labelsScrapper,
 };
