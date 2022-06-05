4f8c5c34e8b2b428e8570643f7b74142350ae115
MatiasMonasterio <matias_monasterio@outlook.com>
2022-06-05 14:59:12 -0300

    feat(workspaces/server): add clear cache function to labels

--- workspaces/server/src/utils/clearCache.ts
+++ workspaces/server/src/utils/clearCache.ts
@@ -5,3 +5,9 @@ export const clearArtistCache = async (): Promise<void> => {
   await cache.del("/api/artists/releases");
   await cache.del("/api/artists/upcomings");
 };
+
+export const clearLabelCache = async (): Promise<void> => {
+  await cache.del("/api/labels");
+  await cache.del("/api/labels/releases");
+  await cache.del("/api/labels/upcomings");
+};
