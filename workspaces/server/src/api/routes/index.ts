a5f89c84db2c2274ff57abae01fc82c8ec28d656
MatiasMonasterio <matias_monasterio@outlook.com>
2022-06-05 15:00:40 -0300

    feat(workspaces/server): add and implement labels routes

--- workspaces/server/src/api/routes/index.ts
+++ workspaces/server/src/api/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import artistRoutes from "./artistRoutes";
+import labelRoutes from "./labelRoutes";
 
 const router = Router();
 
 router.use("/api/artists", artistRoutes);
+router.use("/api/labels", labelRoutes);
 
 export default router;
