9971043f33a4464c21a930dc5ba5fb47093c2d75
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-14 20:26:19 -0300

    feat(workspaces/client): get username and avatar from jwt

--- workspaces/client/src/modules/dashboard/components/Navbar/Account/Account.tsx
+++ workspaces/client/src/modules/dashboard/components/Navbar/Account/Account.tsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
-import { Image, Menu, MenuButton, MenuList, MenuItem, Text } from "@chakra-ui/react";
+import { Menu, MenuButton, MenuList, MenuItem, Text, Avatar } from "@chakra-ui/react";
 import { BiUser, BiSlider, BiLogOut } from "react-icons/bi";
 
+import { useAuthorization } from "contexts/authorization";
+
 export default function Account() {
+  const { jwtDecode, deleteToken } = useAuthorization();
+
+  const handleSignOut = () => {
+    deleteToken();
+  };
+
   return (
     <Menu>
       <MenuButton bg="gray.900" p={1} borderRadius="full" display={{ base: "none", sm: "flex" }}>
-        <Image borderRadius="full" boxSize={8} src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+        <Avatar borderRadius="full" boxSize={8} src={jwtDecode.avatar} name={jwtDecode.username} />
       </MenuButton>
 
       <MenuList
@@ -39,8 +47,7 @@ export default function Account() {
           <Text fontSize="md">Settings</Text>
         </MenuItem>
         <MenuItem
-          as={Link}
-          to="/auth/login"
+          onClick={handleSignOut}
           _focus={{ bgColor: "initial", color: "white" }}
           _hover={{ bgColor: "initial", color: "white" }}
           icon={<BiLogOut />}
