2ce5eeaccf58867c73b65483cf8b4a05af348b8b
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-14 19:47:04 -0300

    feat(workspaces/server): add jwt secret and change cors url name

--- workspaces/server/src/config/constants.ts
+++ workspaces/server/src/config/constants.ts
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT;
-export const CORS_URL = process.env.CORS_URL;
-
+export const CLIENT_URL = process.env.CLIENT_URL;
 export const REDIS_URL = process.env.REDIS_URL;
+export const JWT_SECRET = process.env.JWT_SECRET || "";
