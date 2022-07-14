ec2f65c84c7467286829dc05d743278b164aa928
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-14 20:21:48 -0300

    refactor(workspaces/client): use api interceptor

--- workspaces/client/src/modules/auth/services/auth.ts
+++ workspaces/client/src/modules/auth/services/auth.ts
@@ -1,5 +1,27 @@
-export const login = () => {
-  /** */
+import type { AxiosResponse } from "axios";
+import { api } from "interceptors";
+
+interface Credentials {
+  email: string;
+  password: string;
+}
+
+interface ResponseJson {
+  data: string;
+  error?: string;
+}
+
+export const login = async ({ email, password }: Credentials): Promise<string> => {
+  try {
+    const response: AxiosResponse = await api.post("/api/users/login", { email, password });
+    const { data: token, error }: ResponseJson = response.data;
+    if (error) throw new Error(error);
+
+    return token;
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
 };
 
 export const loginWithGoogle = () => {
