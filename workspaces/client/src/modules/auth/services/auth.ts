0efd85a75a7d49d347860522777358e745acb80c
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-25 13:11:19 -0300

    feat(workspaces/client): add register services

--- workspaces/client/src/modules/auth/services/auth.ts
+++ workspaces/client/src/modules/auth/services/auth.ts
@@ -1,4 +1,3 @@
-import type { AxiosResponse } from "axios";
 import type { UserCredentials } from "@br/core";
 import type { TokenResponse } from "types";
 
@@ -6,8 +5,21 @@ import { api } from "interceptors";
 
 export const login = async ({ email, password }: UserCredentials): Promise<string> => {
   try {
-    const response: AxiosResponse = await api.post("/api/users/login", { email, password });
-    const { data: token, error }: TokenResponse = response.data;
+    const response = await api.post<TokenResponse>("/api/users/login", { email, password });
+    const { data: token, error } = response.data;
+    if (error) throw new Error(error);
+
+    return token;
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
+};
+
+export const register = async ({ email, password }: UserCredentials): Promise<string> => {
+  try {
+    const response = await api.post<TokenResponse>("/api/users/register", { email, password });
+    const { data: token, error } = response.data;
     if (error) throw new Error(error);
 
     return token;
