9e4dd605baf4097ebe47e7bcb5889f88eb750d0b
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-25 13:16:29 -0300

    refactor(workspaces/client): change auth layout style

--- workspaces/client/src/modules/auth/layouts/AuthLayout/AuthLayout.tsx
+++ workspaces/client/src/modules/auth/layouts/AuthLayout/AuthLayout.tsx
@@ -1,5 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { Center } from "@chakra-ui/react";
 
 export default function AuthLayout() {
-  return <Outlet />;
+  return (
+    <Center w="100%" h="100vh">
+      <Center
+        bgColor="secondary.black.800"
+        px={8}
+        py={10}
+        h={{ base: "100vh", sm: "auto" }}
+        flexDir="column"
+        borderRadius="md"
+        width="450px"
+        maxWidth="100%"
+      >
+        <Outlet />
+      </Center>
+    </Center>
+  );
 }
--- workspaces/client/src/modules/auth/pages/Login/Login.tsx
+++ workspaces/client/src/modules/auth/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { Heading, Center, Button, Text, Box } from "@chakra-ui/react";
+import { Heading, Button, Text, Box } from "@chakra-ui/react";
 import { FcGoogle } from "react-icons/fc";
 
 import { useAuthorization } from "contexts/authorization";
@@ -15,38 +15,29 @@ export default function Login() {
   };
 
   return (
-    <Center w="100%" h="100vh">
-      <Center
-        bgColor="secondary.black.800"
-        px={8}
-        py={10}
-        flexDir="column"
-        borderRadius="md"
-        minWidth="450px"
-      >
-        <Box mb={6} textAlign="center">
-          <Heading as="h1" size="lg" fontWeight="medium">
-            Beat Releases
-          </Heading>
+    <>
+      <Box mb={6} textAlign="center">
+        <Heading as="h1" size="lg" fontWeight="medium">
+          Beat Releases
+        </Heading>
 
-          <Text color="primary.green" fontSize="sm">
-            Keep updated!
-          </Text>
-        </Box>
-
-        <Button fontWeight="medium" borderRadius="4px" w="100%" onClick={loginWithGoogle}>
-          <FcGoogle />
-          <Text as="span" fontSize="sm" ml={2} color="secondary.black.900">
-            Sign in with Google
-          </Text>
-        </Button>
-
-        <Text fontSize="xs" my={2}>
-          Or
+        <Text color="primary.green" fontSize="sm">
+          Keep updated!
         </Text>
+      </Box>
 
-        <LoginForm onSubmit={handleSubmit} />
-      </Center>
-    </Center>
+      <Button fontWeight="medium" borderRadius="4px" w="100%" onClick={loginWithGoogle}>
+        <FcGoogle />
+        <Text as="span" fontSize="sm" ml={2} color="secondary.black.900">
+          Sign in with Google
+        </Text>
+      </Button>
+
+      <Text fontSize="xs" my={2}>
+        Or
+      </Text>
+
+      <LoginForm onSubmit={handleSubmit} />
+    </>
   );
 }
