0c28f1e64af9b36f5924e744c563b1c51259b610
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-27 17:33:29 -0300

    refactor(workspaces/client): change function return interface

--- workspaces/client/src/modules/dashboard/services/user.ts
+++ workspaces/client/src/modules/dashboard/services/user.ts
@@ -4,7 +4,7 @@ import type { UserResponse } from "types";
 
 import { api } from "interceptors";
 
-export const getUserInfo = async (): Promise<User | null> => {
+export const getUserInfo = async (): Promise<User> => {
   const response: AxiosResponse = await api.get("/api/users");
   const { data: user, error }: UserResponse = response.data;
   if (error) throw new Error(error);
