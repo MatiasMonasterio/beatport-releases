6109ce6cf28ae8658b90fd73c421f7a91cbbe56d
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-28 17:26:39 -0300

    refactor(workspaces/client): add controls to size

--- workspaces/client/src/modules/dashboard/components/PlayButtonPlaylist/PlayButtonPlaylist.tsx
+++ workspaces/client/src/modules/dashboard/components/PlayButtonPlaylist/PlayButtonPlaylist.tsx
@@ -1,36 +1,46 @@
+import type { ButtonProps } from "@chakra-ui/react";
 import type { Track } from "@br/core";
 
-import { Button, Text } from "@chakra-ui/react";
+import { useMemo } from "react";
+import { Button } from "@chakra-ui/react";
 import { BiPlay } from "react-icons/bi";
 
 import { usePlayerContext } from "@/dashboard/contexts/player";
 
-interface Props {
+interface Props extends ButtonProps {
   playlist: Track[];
+  size?: "md" | "sm";
   disabled?: boolean;
 }
 
-export default function PlayButtonPlaylist({ playlist, disabled }: Props) {
+export default function PlayButtonPlaylist({ playlist, size = "md", disabled, ...args }: Props) {
   const { loadPlaylist } = usePlayerContext();
 
+  const isSmall = useMemo(() => {
+    return size === "sm";
+  }, [size]);
+
   const handlePlay = () => {
-    loadPlaylist({ track: playlist[0], playlist: playlist });
+    loadPlaylist({
+      track: playlist[0],
+      playlist: playlist,
+    });
   };
 
   return (
     <Button
-      bgColor="secondary.gray.200"
-      color="secondary.black.800"
-      gap={4}
+      border="none"
       borderRadius="full"
-      px={3}
-      py={7}
-      onClick={handlePlay}
+      color="secondary.black.800"
+      fontSize={isSmall ? "2xl" : "3xl"}
       disabled={disabled}
+      onClick={handlePlay}
+      px={isSmall ? 0 : 3}
+      py={isSmall ? 0 : 7}
+      _hover={{ transform: "scale(1.04)" }}
+      {...args}
     >
-      <Text as="span" fontSize="3xl">
-        <BiPlay />
-      </Text>
+      <BiPlay />
     </Button>
   );
 }
