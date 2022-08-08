7083efbdce0b052db58909c8f9258598716be857
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-08 10:47:57 -0300

    refactor(workspaces/server): use jwt utility

--- workspaces/server/src/api/middleware/isAuthenticated.ts
+++ workspaces/server/src/api/middleware/isAuthenticated.ts
@@ -1,9 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
-import type { JwtDecode } from "@br/core";
-
-import jwt from "jsonwebtoken";
-
-import { JWT_SECRET } from "../../config/constants";
+import jwt from "../../utils/jwt";
 
 export default async function isAuthenticated(req: Request, res: Response, next: NextFunction) {
   try {
@@ -11,7 +7,7 @@ export default async function isAuthenticated(req: Request, res: Response, next:
 
     if (autorization && autorization.toLocaleLowerCase().startsWith("bearer")) {
       const token = autorization.split(" ")[1];
-      const decode = jwt.verify(token, JWT_SECRET) as JwtDecode;
+      const decode = jwt.verify(token);
 
       if (!token || !decode.id) {
         return res.status(403).send({ status: "FAILD", message: "token missin or invalid" });
