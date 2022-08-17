b714d32349fd57872fda03676afc63f0a5d3e783
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-17 17:06:44 -0300

    feat(workspaces/server): implmenet error handler and route not found middlewares

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -6,28 +6,31 @@ import morgan from "morgan";
 import cors from "cors";
 
 import apiRoutes from "./api";
+import { routeNotFound, handleHttpError } from "./middleware";
 import { connectDatabases } from "./utils";
 import { PORT, CLIENT_URL } from "./config/constants";
 
 const app = express();
 
+app.use(cors({ origin: CLIENT_URL }));
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors({ origin: CLIENT_URL }));
 
-app.get("/", (_req, res) => {
-  res.send("Beat Releases API");
-});
-
+app.get("/", (_req, res) => res.send("Beat Releases API"));
 app.use("/api", apiRoutes);
+app.all("*", routeNotFound);
 
-app.all("*", (req, _res, next) => {
-  next({ status: 404, message: `Can't find ${req.originalUrl} on this server!` });
-});
+app.use(handleHttpError);
 
 connectDatabases()
   .then(() => app.listen(PORT || 3001, () => console.log("Server init")))
   .catch((err) => console.error("Error: " + err));
+
+process.on("uncaughtException", (error: Error) => {
+  console.error(error.name, error.message);
+  console.error("UNHANDLED REJECTION! 💥 Shutting down...");
+  process.exit(1);
+});
 
 process.on("unhandledRejection", (err: Error) => {
   console.error(err.name, err.message);
