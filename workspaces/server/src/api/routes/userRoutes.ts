a19373e1f8bd2c49c7d538b1e50b26fd0453d087
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-18 14:53:19 -0300

    feat(workspaces/server): add user validations

--- workspaces/server/src/api/routes/userRoutes.ts
+++ workspaces/server/src/api/routes/userRoutes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import { isAuthenticated } from "../middleware";
 import { userController } from "../controllers";
+import { userValidator } from "../validators";
 
 const router = Router();
 
 router.get("/", isAuthenticated, userController.getUser);
-router.post("/register", userController.createNewUser);
-router.post("/login", userController.loginUser);
+router.post("/register", userValidator.credentials, userController.createNewUser);
+router.post("/login", userValidator.credentials, userController.loginUser);
 
 export default router;
