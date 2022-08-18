6213a90c631267e2a03e23e73d3b78d3aac371c4
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-18 14:51:16 -0300

    feat(workspaces/server): implement favorite validations

--- workspaces/server/src/api/controllers/favoriteController.ts
+++ workspaces/server/src/api/controllers/favoriteController.ts
@@ -4,8 +4,10 @@ import { sendHttpResponse } from "../../utils";
 import { favoriteService } from "../services";
 
 const getAllFavorites = async (req: Request, res: Response, next: NextFunction) => {
+  const userId = req.userId;
+
   try {
-    const favorites = await favoriteService.getAllFavorites(req.userId);
+    const favorites = await favoriteService.getAllFavorites(userId);
     sendHttpResponse({ data: favorites, res });
   } catch (error) {
     next(error);
@@ -13,10 +15,11 @@ const getAllFavorites = async (req: Request, res: Response, next: NextFunction) 
 };
 
 const createNewFavorite = async (req: Request, res: Response, next: NextFunction) => {
+  const userId = req.userId;
   const track = req.body;
 
   try {
-    const favorite = await favoriteService.createNewFavorite(req.userId, track);
+    const favorite = await favoriteService.createNewFavorite(userId, track);
     sendHttpResponse({ data: favorite, res });
   } catch (error) {
     next(error);
@@ -24,10 +27,11 @@ const createNewFavorite = async (req: Request, res: Response, next: NextFunction
 };
 
 const deleteOneFavorite = async (req: Request, res: Response, next: NextFunction) => {
-  const id = parseInt(req.params.id);
+  const favoriteId = parseInt(req.params.id);
+  const userId = req.userId;
 
   try {
-    await favoriteService.deleteOneFavorite(id, req.userId);
+    await favoriteService.deleteOneFavorite(favoriteId, userId);
     sendHttpResponse({ res });
   } catch (error) {
     next(error);
--- workspaces/server/src/api/routes/favoriteRoutes.ts
+++ workspaces/server/src/api/routes/favoriteRoutes.ts
@@ -2,11 +2,18 @@ import { Router } from "express";
 
 import { isCached, isAuthenticated, updateData } from "../middleware";
 import { favoriteController } from "../controllers";
+import { favoriteValidator } from "../validators";
 
 const router = Router();
 
 router.get("/", isAuthenticated, isCached, updateData, favoriteController.getAllFavorites);
+// todo
 router.post("/", isAuthenticated, favoriteController.createNewFavorite);
-router.delete("/:id", isAuthenticated, favoriteController.deleteOneFavorite);
+router.delete(
+  "/:id",
+  isAuthenticated,
+  favoriteValidator.idParam,
+  favoriteController.deleteOneFavorite
+);
 
 export default router;
