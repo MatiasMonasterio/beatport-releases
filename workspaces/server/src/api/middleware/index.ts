1c9faeee12c1ff572afbe34371ba04260fcc14aa
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-18 14:47:51 -0300

    feat(workspaces/server): add check validations middleware

--- workspaces/server/src/api/middleware/index.ts
+++ workspaces/server/src/api/middleware/index.ts
@@ -2,3 +2,4 @@ export { default as isCached } from "./isCached";
 export { default as isAuthenticated } from "./isAuthenticated";
 export { default as updateData } from "./updateData";
 export { default as isDeveloment } from "./isDevelopment";
+export { default as checkValidation } from "./checkValidatios";
