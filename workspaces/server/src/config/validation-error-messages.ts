a4bb643f6547cdbee67007fb12cdc284c1230639
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-18 14:43:01 -0300

    feat(workspaces/server): add validator error messages

---
+++ workspaces/server/src/config/validation-error-messages.ts
@@ -0,0 +1,11 @@
+const errorMessages = {
+  isRequired: "is required",
+  isString: "must be an string",
+  isEmail: "must be a valid email",
+  isDate: "must be a valid date",
+  isNumeric: "must be a number",
+  notEmpty: "must not be empty",
+  isStrongPassword: "must be a strong",
+};
+
+export default errorMessages;
