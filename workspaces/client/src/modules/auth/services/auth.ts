39d2b14752d1bca38a91b7e920ef7eaaeac04e42
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-30 18:51:33 -0300

    feat(workspaces/client): add logout service

--- workspaces/client/src/modules/auth/services/auth.ts
+++ workspaces/client/src/modules/auth/services/auth.ts
@@ -7,19 +7,25 @@ export const loginWithCredentials = async ({
   email,
   password,
 }: UserCredentials): Promise<string> => {
-  const response = await api.post<TokenResponse>("/api/users/login", { email, password });
+  const response = await api.post<TokenResponse>("/api/auth/login", { email, password });
   const { data: token, error } = response.data;
-  if (error) throw new Error(error);
+  if (error) throw new Error(error.message);
 
   return token;
 };
 
 export const register = async ({ email, password }: UserCredentials): Promise<string> => {
-  const response = await api.post<TokenResponse>("/api/users/register", { email, password });
+  const response = await api.post<TokenResponse>("/api/auth/register", { email, password });
   const { data: token, error } = response.data;
-  if (error) throw new Error(error);
+  if (error) throw new Error(error.message);
 
   return token;
+};
+
+export const logout = async (): Promise<void> => {
+  const response = await api.post("/api/auth/logout");
+  const { error } = response.data;
+  if (error) throw new Error(error.message);
 };
 
 export const loginWithGoogle = () => {
