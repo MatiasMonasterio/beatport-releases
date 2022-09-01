923cb1367d1ffa37f537eadc97b0f5883bc4e3d7
MatiasMonasterio <matias_monasterio@outlook.com>
2022-09-01 18:45:32 -0300

    feat(workspaces/server): ignore dist folder when run tests

--- workspaces/server/jest.config.ts
+++ workspaces/server/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   displayName: "server",
   testEnvironment: "node",
+  modulePathIgnorePatterns: ["./dist"],
 };
 
 export default config;
